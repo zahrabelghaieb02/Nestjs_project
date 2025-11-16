50b1e0d147ba5e793acef10f6708b8590dbca26e
zahrabelghaieb02 <Ghaieb.zahra@esprit.tn>
2025-11-16 14:24:17 +0100

    first commit

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,7 +4,7 @@ import { CreateUserDto } from './dto/create-user.dto/create-user.dto';
 export class UsersController {
   private nbID: number = 4;
   private users = [
-    { id: 1, username: 'Smail', email: 'chemlali@esprit.tn', status: 'active' },
+    { id: 1, username: 'Zahra', email: 'Zahra@esprit.tn', status: 'active' },
     { id: 2, username: 'Sarra', email: 'sarra@esprit.tn', status: 'inactive' },
     { id: 3, username: 'Ali', email: 'ali@esprit.tn', status: 'inactive' },
     { id: 4, username: 'Eya', email: 'eya@esprit.tn', status: 'active' },
